75ac0f17b1f4b33ca9076dd8abc852d951d77e77
Victor Azevedo <josevcazevedo@gmail.com>
2022-12-08 10:54:44 -0300

    fix: birthday validation

--- src/models/customerSchema.models.js
+++ src/models/customerSchema.models.js
@@ -1,8 +1,11 @@
-import Joi from "joi";
+import JoiBase from "joi";
+import JoiDate from "@joi/date";
+
+const Joi = JoiBase.extend(JoiDate);
 
 export const customerSchema = Joi.object({
   name: Joi.string().min(3).required(),
   phone: Joi.string().min(10).max(11).required(),
   cpf: Joi.string().length(11).required(),
-  birthday: Joi.date().required(),
+  birthday: Joi.date().format("YYYY-MM-DD").required(),
 });
