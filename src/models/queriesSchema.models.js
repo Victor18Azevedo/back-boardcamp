0bfb516bfd8a657b3a45a3618e6575c8ff5492b1
Victor Azevedo <josevcazevedo@gmail.com>
2022-12-11 14:37:58 -0300

    feat: update queries schema

--- src/models/queriesSchema.models.js
+++ src/models/queriesSchema.models.js
@@ -1,6 +1,16 @@
-import Joi from "joi";
+import JoiBase from "joi";
+import JoiDate from "@joi/date";
+
+const Joi = JoiBase.extend(JoiDate);
 
 export const queriesSchema = Joi.object({
   customerId: Joi.number().greater(0),
   gameId: Joi.number().greater(0),
+  offset: Joi.number().greater(0),
+  limit: Joi.number().greater(0),
+  order: Joi.string(),
+  desc: Joi.alternatives(["true", "false"]),
+  status: Joi.alternatives(["open", "closed"]),
+  startDate: Joi.date().format("YYYY-MM-DD").max("now"),
+  endDate: Joi.date().format("YYYY-MM-DD").min(Joi.ref("startDate")).max("now"),
 });
